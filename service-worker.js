7b53136e27d9d5661598d65897816bc2993b4e53
balajiapollo2025-gif <balaji.apollo2025@gmail.com>
2026-07-08 10:44:06 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 // IMPORTANT: bump CACHE_NAME (e.g. v6, v7...) every time you publish an
 // update, otherwise returning users keep seeing the old cached version.
 
-const CACHE_NAME = 'chit-funds-cache-v13';
+const CACHE_NAME = 'chit-funds-cache-v14';
 const ASSETS = [
   './',
   './index.html',
